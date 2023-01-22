5245bed1b6fee34b8964a74313139365246970d2
ONUR YURT <oyurt43@gmail.com>
2023-01-22 15:33:29 +0300

    strategy

---
+++ WebApp.Strategy/Migrations/20230121211625_addproductentity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseProject.Migrations
+{
+    public partial class addproductentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Stock = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    CreateDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ WebApp.Strategy/Reporsitories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Strategy.csproj.Models;
+
+namespace WebApp.Strategy.Reporsitories
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetById(string id);
+        Task<List<Product>> GetAllByUserId(string userId);
+        Task<Product> Save(Product product);
+        Task Update(Product product);
+        Task Delete(Product product);
+
+    }
+}
